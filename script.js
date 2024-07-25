d750bed0b77483d8976bf1e3f9ae44c8ef6b7bd0
Yan Kolev <y.kolev11@gmail.com>
2024-07-25 14:48:08 +0200

    Applying class acrhitecture vol.2

--- script.js
+++ script.js
@@ -13,7 +13,55 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 //Geolocation API 
 //adding global variables
-let map, mapEvent 
+//let map, mapEvent 
+
+class Workout {
+    date = new Date();
+    id = (Date.now () + '').slice(-10); 
+
+
+    constructor(coords, distance, duration){
+        
+        this.coords = coords; // array of attitude and longitude [lat, log]
+        this.distance = distance; // in km
+        this.duration = duration; // in min
+    }
+}
+
+class Running extends Workout {
+    constructor (coords, distance, duration, cadence) {
+        super(coords, distance, duration);
+        this.cadence = cadence;
+        this.calcPace(); 
+    }
+
+    calcPace(){
+        // defined->min/km
+        this.pace = this.duration / this.distance;
+        return this.pace
+    }
+}
+class Cycling extends Workout {
+    constructor (coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed(); //its ok to call a method in the constructor, AS LONG as its not written inside / needs to be outside
+    }
+
+    calcSpeed(){
+        //km/h 
+        this.speed = this.distance / (this.duration / 60);
+        return this.speed
+    }
+}
+
+
+//const run1 = new Running([39, -12], 5.2, 24, 178)
+//const cycling1 = new Cycling ([39, -12], 27, 95, 523)
+//console.log(run1, cycling1);
+
+////////////////////////////////////////////////////////////////
+//Application Architecture
 
 class App {
     #map;
